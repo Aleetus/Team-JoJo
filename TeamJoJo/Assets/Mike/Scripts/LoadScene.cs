4ecd9b19ceb6378765e80d9ffb79a4b51b20ba32
Aleetus <Aleetus@users.noreply.github.com>
2020-02-06 19:33:26 +0000

    Started work on menu/UI
    Started work on menu/UI

---
+++ TeamJoJo/Assets/Mike/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public string Load_Scene;
+
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene(Load_Scene);
+    }
+}
