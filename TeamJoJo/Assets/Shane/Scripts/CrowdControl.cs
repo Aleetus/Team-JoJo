6034d0fbbd117bd8bc6ca10ff3291c39a5dddb6f
Shizzy-223 <ShaneFitzy15@live.co.uk>
2020-02-12 15:16:02 +0000

    Player

---
+++ TeamJoJo/Assets/Shane/Scripts/CrowdControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdControl : MonoBehaviour
+{
+    public GameObject go_target;
+    public float fl_range = 10;
+    public float fl_speed = 1;
+    CharacterController cc_NPC;
+
+    // ----------------------------------------------------------------------
+    // Use this for initialization
+    void Start()
+    {
+
+        cc_NPC = GetComponent<CharacterController>();
+        go_target = GameObject.Find("NPC_Crowd Manager");
+
+    }//-----
+
+    // ----------------------------------------------------------------------
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(go_target.transform.position, transform.position) > fl_range)
+        {
+            transform.LookAt(go_target.transform.position);
+            cc_NPC.SimpleMove(2 * fl_speed * transform.TransformDirection(Vector3.forward));
+
+        }
+        else if (Vector3.Distance(go_target.transform.position, transform.position) <= fl_range)
+        {
+            cc_NPC.SimpleMove(fl_speed * transform.TransformDirection(Vector3.forward));
+
+        }
+
+        if ((cc_NPC.collisionFlags & CollisionFlags.Sides) != 0) transform.Rotate(0, Random.Range(-180, 180), 0);
+    }
+}
---
+++ TeamJoJo/Assets/Shane/Scripts/PushObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushObject : MonoBehaviour
+{
+    public float fl_force = 10;
+
+    // ----------------------------------------------------------------------
+    // Char Controller has Hit something 
+    void OnControllerColliderHit(ControllerColliderHit _CC_Hit)
+    {
+        // Has the PC bumped into an object it can push 
+        if (_CC_Hit.collider.attachedRigidbody && _CC_Hit.moveDirection.y > -0.2)
+        {
+            // Convert the local Character direction to world coords
+            Vector3 _V3_direction = transform.TransformDirection(Vector3.forward);
+
+            // Apply the force to the object
+            _CC_Hit.collider.attachedRigidbody.AddForce(fl_force * new Vector3(_V3_direction.x, 0, _V3_direction.z));
+        }
+    }
+}
