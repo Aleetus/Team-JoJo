ea942d32a5532a694008386fc8635b3da737d25b
Michael Wiggins <Aleetus@users.noreply.github.com>
2020-02-14 15:33:29 +0000

    mute button bleh

---
+++ TeamJoJo/Assets/Mike/Scripts/MuteButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteButton : MonoBehaviour
+{
+    public GameObject muted;
+    public GameObject unmuted;
+
+    bool isMute;
+
+    public void Mute()
+    {
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+
+        if (isMute == true)
+        {
+            muted.SetActive(true);
+            unmuted.SetActive(false);
+        }
+        
+        if (isMute == false)
+        {
+            muted.SetActive(false);
+            unmuted.SetActive(true);
+        }
+
+    }
+}
