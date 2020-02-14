dc30df223d53523b1ee31f0656c656007c665f6b
Michael Wiggins <Aleetus@users.noreply.github.com>
2020-02-14 22:07:34 +0000

    Menu is banging boi

---
+++ TeamJoJo/Assets/Mike/Scripts/Clouds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+    public float movementSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = this.transform.position + new Vector3(movementSpeed, 0, 0);
+    }
+}
---
+++ TeamJoJo/Assets/Mike/Scripts/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+
+    public float rotateSpeed = 1.5f;
+
+    void Update()
+    {
+        transform.Rotate(0f, rotateSpeed, 0f);
+    }
+
+}
