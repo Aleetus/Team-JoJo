3ff959fccf6f94dda430c7c65bae7cb8c3321d14
Shizzy-223 <58072496+Shizzy-223@users.noreply.github.com>
2020-03-11 18:17:22 +0000

    I'm Commiting to Master

--- TeamJoJo/Assets/Shane/Scripts/Movement.cs
+++ TeamJoJo/Assets/Shane/Scripts/Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
+    public bool stopMovement;
     public float fl_speed = 6.0F;
     public float fl_jump_force = 8.0F;
     public float fl_gravity = 20.0F;
@@ -41,6 +41,7 @@ public class Movement : MonoBehaviour
         else
             fl_speed = fl_initial_speed;
 
+
         //  PC Ground Movement
         if (CC_PC.isGrounded)
         {
@@ -52,46 +53,54 @@ public class Movement : MonoBehaviour
 
             // if the jump key is pressed add jump force to the direction vector
             if (Input.GetButtonDown("Jump")) V3_move_direction.y = fl_jump_force;
+
+          
         }
 
 
 
-        // PC Climb Movement
-        if (bl_climbing)
+
         {
-            // Add Y movement to the direction vector based on Vertical input (W,S or Cursor U,D) 
-            V3_move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            V3_move_direction = fl_speed / 2 * transform.TransformDirection(V3_move_direction);
-            if (Input.GetButton("Jump")) V3_move_direction.y = fl_speed;
+
+
+
+            // PC Climb Movement
+            if (bl_climbing)
+            {
+                // Add Y movement to the direction vector based on Vertical input (W,S or Cursor U,D) 
+                V3_move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                V3_move_direction = fl_speed / 2 * transform.TransformDirection(V3_move_direction);
+                if (Input.GetButton("Jump")) V3_move_direction.y = fl_speed;
+            }
+            else
+            {   // Add fl_gravity to the direction vector
+
+
+                V3_move_direction.y -= fl_gravity * Time.deltaTime;
+            }
+
+            // Move the character controller with the direction vector
+            CC_PC.Move(V3_move_direction * Time.deltaTime);
+
+        }// -----
+
+
+        //-------------------------------------------------------------------------
+        // When PC enters a trigger
+        void OnTriggerStay(Collider cl_trigger_collider)
+        {
+            if (cl_trigger_collider.gameObject.tag == "Moving") transform.parent = cl_trigger_collider.transform;
+            if (cl_trigger_collider.gameObject.tag == "ClimbZone") bl_climbing = true;
+        }//-----
+
+
+        //-------------------------------------------------------------------------
+        // PC Leaving the Trigger
+        void OnTriggerExit(Collider cl_trigger_collider)
+        {
+            if (cl_trigger_collider.gameObject.tag == "Moving") transform.parent = null;
+            if (cl_trigger_collider.gameObject.tag == "ClimbZone") bl_climbing = false;
         }
-        else
-        {   // Add fl_gravity to the direction vector
 
-
-            V3_move_direction.y -= fl_gravity * Time.deltaTime;
-        }
-
-        // Move the character controller with the direction vector
-        CC_PC.Move(V3_move_direction * Time.deltaTime);
-
-    }// -----
-
-
-    //-------------------------------------------------------------------------
-    // When PC enters a trigger
-    void OnTriggerStay(Collider cl_trigger_collider)
-    {
-        if (cl_trigger_collider.gameObject.tag == "Moving") transform.parent = cl_trigger_collider.transform;
-        if (cl_trigger_collider.gameObject.tag == "ClimbZone") bl_climbing = true;
-    }//-----
-
-
-    //-------------------------------------------------------------------------
-    // PC Leaving the Trigger
-    void OnTriggerExit(Collider cl_trigger_collider)
-    {
-        if (cl_trigger_collider.gameObject.tag == "Moving") transform.parent = null;
-        if (cl_trigger_collider.gameObject.tag == "ClimbZone") bl_climbing = false;
     }
-
 }
