4a8bba94bcd62d1f8492c3a28b4dce585fd3bcfa
Michael Wiggins <Aleetus@users.noreply.github.com>
2020-03-15 15:41:57 +0000

    level design bits and bobs

---
+++ TeamJoJo/Assets/LeafDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafDestroy : MonoBehaviour
+{
+    void Start()
+    {
+
+        Destroy(gameObject, 8);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
