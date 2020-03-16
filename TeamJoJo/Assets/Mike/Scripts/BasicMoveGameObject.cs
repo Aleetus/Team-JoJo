8719e07087fda63835c8da7dbbdf735a022f39cc
Michael Wiggins <Aleetus@users.noreply.github.com>
2020-03-16 23:06:51 +0000

    fff

---
+++ TeamJoJo/Assets/Mike/Scripts/BasicMoveGameObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMoveGameObject : MonoBehaviour
+{
+
+    public float movementSpeedX = 0.1f;
+    public float movementSpeedY = 0.1f;
+    public float movementSpeedZ = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.x < 50)
+        {
+            this.transform.position = this.transform.position + new Vector3(movementSpeedX, 0, 0);
+        }
+
+        if (this.transform.position.y < 50)
+        {
+            this.transform.position = this.transform.position + new Vector3(0, movementSpeedY, 0);
+        }
+
+        if (this.transform.position.z < 50)
+        {
+            this.transform.position = this.transform.position + new Vector3(0, 0, movementSpeedZ);
+        }
+
+    }
+}
