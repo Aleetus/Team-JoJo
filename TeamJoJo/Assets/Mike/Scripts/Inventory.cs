4f26155d40d71542f276d5cbcd8c38a23b79c20d
Michael Wiggins <Aleetus@users.noreply.github.com>
2020-03-17 19:48:45 +0000

    moreee

---
+++ TeamJoJo/Assets/Mike/Scripts/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    PickUp currentObject;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentObject != null)
+        {
+            Debug.Log("hah yeah");
+            text.text = "haha yeah";
+        }
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("here");
+        currentObject = other.gameObject.GetComponent<PickUp>();
+    }
+}
---
+++ TeamJoJo/Assets/Mike/Scripts/PickUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public AudioClip sound;
+    public bool hasApple;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            print("Item picked up");
+            Destroy(gameObject);
+            hasApple = true;
+        }
+    }
+
+}
