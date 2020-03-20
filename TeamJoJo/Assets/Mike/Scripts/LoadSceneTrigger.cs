599a254ca4965bd0f5a1dd0349879eb551342a7d
Michael Wiggins <Aleetus@users.noreply.github.com>
2020-03-20 12:32:06 +0000

    level structure complete

---
+++ TeamJoJo/Assets/Mike/Scripts/LoadSceneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneTrigger : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+
+}
